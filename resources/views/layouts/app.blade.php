a1434bb09c99e1eb8ad15396ebeed7ef4a2f9393
suzan-naeem <64129170+suzan-naeem@users.noreply.github.com>
2021-11-01 08:13:48 +0200

    ups system

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,22 +26,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
-                {{-- <a class="navbar-brand" href="{{route('posts.index')}}">
-                    All Posts
-                </a> --}}
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    
-                </div>
             </div>
         </nav>
 
